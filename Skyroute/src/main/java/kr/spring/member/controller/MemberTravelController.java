d1975480e21e633cc07a858256bef18b2587b971
ryurim-02 <fbfla1104@gamil.com>
2026-07-13 12:38:20 +0900

    사용자 헤더 여행탭 추가

---
+++ Skyroute/src/main/java/kr/spring/member/controller/MemberTravelController.java
@@ -0,0 +1,43 @@
+package kr.spring.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member/travel")
+public class MemberTravelController {
+
+	@GetMapping("/airport")
+	public String airport(Model model) {
+		model.addAttribute("activeMenu", "travel");
+		model.addAttribute("activeTravel", "airport");
+
+		return "thviews/member/travel/member_airport";
+	}
+
+	@GetMapping("/cabin")
+	public String cabin(Model model) {
+		model.addAttribute("activeMenu", "travel");
+		model.addAttribute("activeTravel", "cabin");
+
+		return "thviews/member/travel/member_cabin";
+	}
+
+	@GetMapping("/aircraft")
+	public String aircraft(Model model) {
+		model.addAttribute("activeMenu", "travel");
+		model.addAttribute("activeTravel", "aircraft");
+
+		return "thviews/member/travel/member_aircraft";
+	}
+
+	@GetMapping("/service")
+	public String service(Model model) {
+		model.addAttribute("activeMenu", "travel");
+		model.addAttribute("activeTravel", "service");
+
+		return "thviews/member/travel/member_service";
+	}
+}
